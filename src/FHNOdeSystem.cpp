3bf13dc9e5ccd1659c8c0b42ac0429075daa11a6
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-10-02 09:59:31 +1300

    Added type to beta variable

--- src/FHNOdeSystem.cpp
+++ src/FHNOdeSystem.cpp
@@ -18,7 +18,7 @@ FHNOdeSystem::FHNOdeSystem() : AbstractOdeSystem(2)
 void FHNOdeSystem::EvaluateYDerivatives(double time, 
 	const std::vector<double>& rY, std::vector<double>& rDY)
 {
-	beta = mC * sin(2*M_PI*mFreq*time); // Define the beta function
+	double beta = mC * sin(2*M_PI*mFreq*time); // Define the beta function
 
 	// rY[0] = u and rY[1] = r
 	rDY[0] = mA*rY[0] * (rY[0] - beta) * (mDelta - rY[0]) - mB * rY[1];
