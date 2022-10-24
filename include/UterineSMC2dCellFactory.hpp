2ab775904a0db8860cac4395a39f0587d597e6e0
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-10-25 11:15:18 +1300

    Fixed UterineSMC2dCellFactory to compile without causing errors

--- include/UterineSMC2dCellFactory.hpp
+++ include/UterineSMC2dCellFactory.hpp
@@ -1,10 +1,8 @@
 #ifndef UTERINESMC2DCELLFACTORY_HPP
 #define UTERINESMC2DCELLFACTORY_HPP
 
-
 #include "MonodomainProblem.hpp"
 #include "SimpleStimulus.hpp"
-#include "PetscSetupAndFinalize.hpp"
 #include "HodgkinHuxley1952.hpp"
 
 class UterineSMC2dCellFactory : public AbstractCardiacCellFactory<2>
@@ -13,8 +11,27 @@ private:
     boost::shared_ptr<SimpleStimulus> mpStimulus;
 
 public:
-	UterineSMC2dCellFactory();
-    AbstractCardiacCell* CreateUterineCellForTissueNode(Node<2>* pNode);
+	UterineSMC2dCellFactory() : 
+		AbstractCardiacCellFactory<2>(),
+		mpStimulus(new SimpleStimulus(-5e5, 0.5))
+	{
+	}
+
+    AbstractCardiacCell* CreateCardiacCellForTissueNode(Node<2>* pNode)
+	{
+		double x = pNode->rGetLocation()[0];
+		double y = pNode->rGetLocation()[1];
+		// if x<=0.02 and y<=0.02 
+		if (x < 0.02+1e-6 && y < 0.02+1e-6) 
+		{
+			return new CellHodgkinHuxley1952FromCellML(mpSolver, mpStimulus);
+		}
+		else
+		{
+			/* The other cells have zero stimuli. */
+			return new CellHodgkinHuxley1952FromCellML(mpSolver, mpZeroStimulus);
+		}
+	}
 };
 
 #endif // UTERINESMC2DCELLFACTORY_HPP
