80649115ebb7014c2da078ac6129f3b74511c1b7
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-10-25 11:54:26 +1300

    Separated the source from the header

--- include/UterineSMC2dCellFactory.hpp
+++ include/UterineSMC2dCellFactory.hpp
@@ -11,27 +11,9 @@ private:
     boost::shared_ptr<SimpleStimulus> mpStimulus;
 
 public:
-	UterineSMC2dCellFactory() : 
-		AbstractCardiacCellFactory<2>(),
-		mpStimulus(new SimpleStimulus(-5e5, 0.5))
-	{
-	}
+	UterineSMC2dCellFactory();
+    AbstractCardiacCell* CreateCardiacCellForTissueNode(Node<2>* pNode);
 
-    AbstractCardiacCell* CreateCardiacCellForTissueNode(Node<2>* pNode)
-	{
-		double x = pNode->rGetLocation()[0];
-		double y = pNode->rGetLocation()[1];
-		// if x<=0.02 and y<=0.02 
-		if (x < 0.02+1e-6 && y < 0.02+1e-6) 
-		{
-			return new CellHodgkinHuxley1952FromCellML(mpSolver, mpStimulus);
-		}
-		else
-		{
-			/* The other cells have zero stimuli. */
-			return new CellHodgkinHuxley1952FromCellML(mpSolver, mpZeroStimulus);
-		}
-	}
 };
 
 #endif // UTERINESMC2DCELLFACTORY_HPP
