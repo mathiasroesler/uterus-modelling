b6317ffd50bd6611a162dcb643313d90e865bdb9
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-10-25 14:17:31 +1300

    Passed UterineSMC2dCellFactory test

--- test/TestUterineSMC2dCellFactory.hpp
+++ test/TestUterineSMC2dCellFactory.hpp
@@ -2,28 +2,8 @@
 #define TESTUTERINESMC2DCELLFACTORY_HPP_
 
 #include <cxxtest/TestSuite.h>
-/* Most Chaste code uses PETSc to solve linear algebra problems.  This involves starting PETSc at the beginning of a test-suite
- * and closing it at the end.  (If you never run code in parallel then it is safe to replace PetscSetupAndFinalize.hpp with FakePetscSetup.hpp)
- */
 #include "PetscSetupAndFinalize.hpp"
 #include "UterineSMC2dCellFactory.hpp"
-
-/**
- * @file
- *
- * This is an example of a CxxTest test suite, used to test the source
- * code, and also used to run simulations (as it provides a handy
- * shortcut to compile and link against the correct libraries using scons).
- *
- * You can #include any of the files in the project 'src' folder.
- * For example here we #include "Hello.hpp"
- *
- * You can utilise any of the code in the main the Chaste trunk
- * in exactly the same way.
- * NOTE: you will have to alter the project SConscript file lines 41-44
- * to enable #including of code from the 'heart', 'cell_based' or 'crypt'
- * components of Chaste.
- */
 
 class TestUterineSMC2dCellFactory : public CxxTest::TestSuite
 {
@@ -35,11 +15,26 @@ public:
         HeartConfig::Instance()->SetOutputDirectory("MonodomainTutorial");
         HeartConfig::Instance()->SetOutputFilenamePrefix("results");
 
-        HeartConfig::Instance()->SetVisualizeWithMeshalyzer(true);
-        HeartConfig::Instance()->SetVisualizeWithCmgui(true);
         HeartConfig::Instance()->SetVisualizeWithVtk(true);
 
 		UterineSMC2dCellFactory factory;
+
+        MonodomainProblem<2> monodomain_problem( &factory );
+
+        HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(1.75, 0.19));
+        HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(6.2, 2.4));
+
+        HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(1400); // 1/cm
+        HeartConfig::Instance()->SetCapacitance(1.0); // uF/cm^2
+
+        HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.01, 0.01, 0.1);
+
+        monodomain_problem.Initialise();
+
+        monodomain_problem.Solve();
+
+        HeartEventHandler::Headings();
+        HeartEventHandler::Report();
     }
 };
 
