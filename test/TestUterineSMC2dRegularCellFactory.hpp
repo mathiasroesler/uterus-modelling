a2ac470a07c496e77b2dc1e92f79538a1f62d839
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-11-10 11:54:15 +1300

    Implemented a test for the new uSMC cell factory class using a regular stimulus

---
+++ test/TestUterineSMC2dRegularCellFactory.hpp
@@ -0,0 +1,45 @@
+#ifndef TESTUTERINESMC2DREGULARCELLFACTORY_HPP_
+#define TESTUTERINESMC2DREGULARCELLFACTORY_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "PetscSetupAndFinalize.hpp"
+#include "UterineSMC2dRegularCellFactory.hpp"
+
+class TestUterineSMC2dRegularCellFactory : public CxxTest::TestSuite
+{
+public:
+    void TestUterineSMC2dRegularCellFactoryClass()
+    {
+		HeartConfig::Instance()->SetSimulationDuration(5.0); //ms
+        HeartConfig::Instance()->SetMeshFileName("mesh/test/data/2D_0_to_1mm_800_elements");
+        HeartConfig::Instance()->SetOutputDirectory("MonodomainTest");
+        HeartConfig::Instance()->SetOutputFilenamePrefix("results");
+
+        HeartConfig::Instance()->SetVisualizeWithVtk(true);
+
+		UterineSMC2dRegularCellFactory factory;
+
+		factory.PrintParams();
+
+        MonodomainProblem<2> monodomain_problem( &factory );
+
+        HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(1.75, 0.19));
+        HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(6.2, 2.4));
+
+        HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(1400); // 1/cm
+        HeartConfig::Instance()->SetCapacitance(1.0); // uF/cm^2
+
+        HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.01, 0.01, 0.1);
+
+        monodomain_problem.Initialise();
+
+        monodomain_problem.Solve();
+
+        HeartEventHandler::Headings();
+        HeartEventHandler::Report();
+    }
+};
+
+#endif /*TESTUTERINESMC2DREGULARCELLFACTORY_HPP_*/
+
+
