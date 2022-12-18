f4722e1cf28d5344090596a28aba684b6b78fcf8
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-12-19 12:20:35 +1300

    Created a class with zero stimulus to all cell in 2D model

---
+++ include/UterineSMC2dZeroCellFactory.hpp
@@ -0,0 +1,22 @@
+#ifndef UTERINESMC2DZEROCELLFACTORY_HPP
+#define UTERINESMC2DZEROCELLFACTORY_HPP
+
+#include <toml.hpp>
+#include "UterineSMC2dCellFactory.hpp"
+#include "MonodomainProblem.hpp"
+#include "ZeroStimulus.hpp"
+
+class UterineSMC2dZeroCellFactory : public UterineSMC2dCellFactory
+{
+private:
+    boost::shared_ptr<ZeroStimulus> mpStimulus;
+
+public:
+	UterineSMC2dZeroCellFactory();
+    AbstractCardiacCell* CreateCardiacCellForTissueNode(Node<2>* pNode);
+	void PrintParams();
+};
+
+#endif // UTERINESMC2DZEROCELLFACTORY_HPP
+
+
---
+++ src/UterineSMC2dZeroCellFactory.cpp
@@ -0,0 +1,24 @@
+#include "UterineSMC2dZeroCellFactory.hpp"
+#include "Exception.hpp"
+
+UterineSMC2dZeroCellFactory::UterineSMC2dZeroCellFactory() : 
+	UterineSMC2dCellFactory(), 
+	mpStimulus(new ZeroStimulus())
+{
+}
+
+
+AbstractCardiacCell* UterineSMC2dZeroCellFactory::CreateCardiacCellForTissueNode(
+	Node<2>* pNode)
+{
+	return UterineSMC2dCellFactory::CreateCardiacCellForTissueNode(pNode);
+}
+
+
+void UterineSMC2dZeroCellFactory::PrintParams()
+{
+	UterineSMC2dCellFactory::PrintParams();
+	std::cout << "Zero stimulus" << std::endl;
+}
+
+
