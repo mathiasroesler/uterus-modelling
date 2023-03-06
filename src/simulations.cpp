ceecb64ea0f24d664e1283465aa3e55ff4ea9e86
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2023-03-06 14:20:12 +1300

    Updated the 3d simulation function

--- src/simulations.cpp
+++ src/simulations.cpp
@@ -154,7 +154,7 @@ void simulation_3d()
 {
 	// Get parameters from config file
 	const auto sys_params = toml::parse(
-		USMC_3D_SYSTEM_CONSTANTS::CONFIG_PATH);
+		USMC_3D_SYSTEM_CONSTANTS::GENERAL_PARAM_FILE);
 
 	// Time constants
 	const double sim_duration = toml::find<double>(sys_params, "sim_duration");
