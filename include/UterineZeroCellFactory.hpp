33867130b79325e9841ad19f2bae107e6050a848
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2023-04-18 09:21:40 +1200

    Implemented the ReadParams method for the 2D zero cell factory

--- include/UterineZeroCellFactory.hpp
+++ include/UterineZeroCellFactory.hpp
@@ -14,6 +14,7 @@ private:
 public:
 	UterineZeroCellFactory();
     AbstractCardiacCell* CreateCardiacCellForTissueNode(Node<2>* pNode);
+	void ReadParams(std::string general_param_file);
 	void ReadCellParams(std::string cell_param_file);
 	void PrintParams();
 	void WriteLogInfo(std::string log_file);
